ccf934eabf2190c10bb0605ace3ea41a677b3e81
OzJasonGit <108492612+OzJasonGit@users.noreply.github.com>
2022-11-24 18:27:18 +0000

    movie images added

--- index.js
+++ index.js
@@ -50,15 +50,23 @@ let movieData = {
     cast: ["Jason Schwartzman", "Owen Wilson", "Adrien Brody"],
     runtime: 151,
     rating: 7.2,
+    imgUrl: '',
     year: 2007,
   },
+
+
+
   "The Royal Tenenbaums": {
     plot: "The eccentric members of a dysfunctional family reluctantly gather under the same roof for various reasons",
     rating: 7.6,
+    imgUrl: '',
     year: 2001,
     cast: ["Gene Hackman", "Gwnyeth Paltrow", "Anjelica Huston"],
     runtime: 170,
   },
+
+
+
   "Fantastic Mr. Fox": {
     year: 2009,
     plot: "An urbane fox cannot resist returning to his farm raiding ways and then must help his community survive the farmers' retaliation.",
@@ -70,9 +78,14 @@ let movieData = {
     ],
     runtime: 147,
     rating: 7.9,
+    imgUrl: '',
   },
+
+
+
   "The Grand Budapest Hotel": {
     rating: 8.1,
+    imgUrl: '',
     runtime: 159,
     year: 2014,
     plot: "A writer encounters the owner of an aging high-class hotel, who tells him of his early years serving as a lobby boy in the hotel's glorious years under an exceptional concierge.",
